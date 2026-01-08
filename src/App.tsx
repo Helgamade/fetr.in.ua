7ec19eea4ef3a9dd0d9c87cfb04bd29fc960a6e0
Oleksandr P. <pitalov@gmail.com>
2026-01-08 05:51:46 +0200

    Fix: disable old AnalyticsProvider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,6 @@ const App = () => (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <CartProvider>
-          <AnalyticsProvider>
             <TooltipProvider>
               <Toaster />
               <Sonner />
